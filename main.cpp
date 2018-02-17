64d1b6a48bacdab0b18cbe1987215f28b800e1de
Daniel Hornik <daniel.1302@gmail.com>
2018-02-17 18:06:42 +0100

    Skeleton

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <SFML/Graphics.hpp>
+
+#include <SFML/Window.hpp>
+
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1024, 768), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
