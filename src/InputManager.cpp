14fb85203e713ed772ae5efbb9d326cab82e592f
Daniel Hornik <daniel.1302@gmail.com>
2018-02-27 23:00:23 +0100

    Input manager

--- src/InputManager.cpp
+++ src/InputManager.cpp
@@ -1,4 +1,30 @@
-//
-// Created by daniel on 27.02.18.
-//
+#include "InputManager.h"
 
+bool InputManager::isSpriteClicked(
+        sf::Sprite object,
+        sf::Mouse::Button button,
+        sf::RenderWindow &window
+)
+{
+    if (sf::Mouse::isButtonPressed(button))
+    {
+        sf::IntRect tempRect(
+                object.getPosition().x,
+                object.getPosition().y,
+                object.getGlobalBounds().width,
+                object.getGlobalBounds().height
+        );
+
+        if (tempRect.contains(sf::Mouse::getPosition(window)))
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+sf::Vector2i InputManager::getMousePosition(sf::RenderWindow &window)
+{
+    return sf::Mouse::getPosition(window);
+}
