c0e4d1c8e963bfdd73ba21d866042590c0b8184f
Daniel Hornik <daniel.1302@gmail.com>
2018-03-04 23:06:43 +0100

    Add tests for ImageAdjuster

---
+++ tests/src/Math/ImageAdjusterTest.cpp
@@ -0,0 +1,90 @@
+#include <gtest/gtest.h>
+#include "Math/ImageAdjuster.h"
+
+TEST(ImageAdjusterTest, InitState) {
+    ImageAdjuster a;
+
+    sf::Vector2f scale = a.scale();
+
+    ASSERT_FLOAT_EQ(1, scale.x);
+    ASSERT_FLOAT_EQ(1, scale.y);
+};
+
+TEST(ImageAdjusterTest, NoScale) {
+    ImageAdjuster a = ImageAdjuster(
+            sf::Vector2f(100, 100),
+            sf::Vector2f(100, 100)
+    );
+
+    sf::Vector2f scale = a.scale(false);
+
+
+    ASSERT_FLOAT_EQ(1, scale.x);
+    ASSERT_FLOAT_EQ(1, scale.y);
+
+    a.setScreenSize(sf::Vector2f(200, 200));
+    a.setImageSize(sf::Vector2f(199, 199));
+
+    scale = a.scale(false);
+
+    ASSERT_FLOAT_EQ(1, scale.x);
+    ASSERT_FLOAT_EQ(1, scale.y);
+}
+
+TEST(ImageAdjusterTest, ScaleWhenScreenWidthIsGreaterThanHeight) {
+    ImageAdjuster a = ImageAdjuster(
+            sf::Vector2f(100, 50),
+            sf::Vector2f(200, 100)
+    );
+
+    sf::Vector2f scale = a.scale(false);
+    ASSERT_FLOAT_EQ(0.5, scale.x);
+    ASSERT_FLOAT_EQ(0.5, scale.y);
+
+
+    a.setImageSize(sf::Vector2f(100, 100));
+    scale = a.scale(false);
+    ASSERT_FLOAT_EQ(0.5, scale.x);
+    ASSERT_FLOAT_EQ(0.5, scale.y);
+
+
+    a.setImageSize(sf::Vector2f(50, 200));
+    scale = a.scale(false);
+    ASSERT_FLOAT_EQ(0.25, scale.x);
+    ASSERT_FLOAT_EQ(0.25, scale.y);
+
+
+    a.setImageSize(sf::Vector2f(500, 10));
+    scale = a.scale(false);
+    ASSERT_FLOAT_EQ(0.2, scale.x);
+    ASSERT_FLOAT_EQ(0.2, scale.y);
+}
+
+TEST(ImageAdjusterTest, ScaleWhenScreenHeightIsGreaterThanWidth) {
+    ImageAdjuster a = ImageAdjuster(
+            sf::Vector2f(50, 100),
+            sf::Vector2f(200, 100)
+    );
+
+    sf::Vector2f scale = a.scale(false);
+    ASSERT_FLOAT_EQ(0.25, scale.x);
+    ASSERT_FLOAT_EQ(0.25, scale.y);
+
+
+    a.setImageSize(sf::Vector2f(100, 100));
+    scale = a.scale(false);
+    ASSERT_FLOAT_EQ(0.5, scale.x);
+    ASSERT_FLOAT_EQ(0.5, scale.y);
+
+
+    a.setImageSize(sf::Vector2f(50, 400));
+    scale = a.scale(false);
+    ASSERT_FLOAT_EQ(0.25, scale.x);
+    ASSERT_FLOAT_EQ(0.25, scale.y);
+
+
+    a.setImageSize(sf::Vector2f(500, 10));
+    scale = a.scale(false);
+    ASSERT_FLOAT_EQ(0.1, scale.x);
+    ASSERT_FLOAT_EQ(0.1, scale.y);
+}
